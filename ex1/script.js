fbda1ab04353a5a2252456d1d9f4d715017c18c2
Xusniddin <narkulof@gmail.com>
2022-02-24 12:18:53 +0500

    alskdj

---
+++ ex1/script.js
@@ -0,0 +1,13 @@
+let vazn = 70;
+let height = 1.83;
+let a = vazn / height ** 2;
+console.log(a);
+console.log(18.5 * height * height);
+
+let vaznNeo = 60;
+let heightNeo = 1.7;
+let b = vaznNeo / heightNeo ** 2;
+console.log(b);
+
+let c = a < b;
+console.log(c);
---
+++ ex2/script.js
@@ -0,0 +1,8 @@
+// 1-topshiriq
+let calcAverage = (a, b, c) => {
+  return (a + b + c) / 3;
+};
+console.log(calcAverage(44, 23, 71));
+console.log(calcAverage(65, 54, 49));
+
+let checkWinner = function(avgDolhins,avgKoalas);
