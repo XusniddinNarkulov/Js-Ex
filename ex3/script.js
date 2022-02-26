8bb305f4591a6dc7b0c0f08f9da5fd1438403a76
Xusniddin <narkulof@gmail.com>
2022-02-26 11:45:27 +0500

    ex3-1 fixed

--- ex3/script.js
+++ ex3/script.js
@@ -1,4 +1,4 @@
-let delfin1 = 96;
+let delfin1 = 91;
 let delfin2 = 108;
 let delfin3 = 89;
 
@@ -10,23 +10,18 @@ let avgDelfin = (delfin1 + delfin2 + delfin3) / 3;
 let avgKoala = (koala1 + koala2 + koala3) / 3;
 console.log(avgDelfin);
 console.log(avgKoala);
-if (avgDelfin > avgKoala) {
+if (avgDelfin >= 100 && avgDelfin > avgKoala) {
   console.log(`delfinlar g'alaba qozondi`);
-} else if (avgDelfin === avgKoala) {
-  console.log(durrang);
+} else if (avgKoala >= 100 && avgKoala > avgDelfin) {
+  console.log(`koalalar g'alaba qozondi`);
 } else {
-  `koalalar g'alaba qozondi`;
+  console.log(`hech kim g'alaba qozonmadi`);
 }
-// if (avgDelfin >= 100 > avgKoala) {
-//   console.log(`delfinlar g'alaba qozondi`);
-// } else {
-//   console.log(`hech kim g'alaba qozonmadi`);
-// }
 
 // ex-2
-let userLogin = prompt(`loginni kiriting`);
-let userParol = prompt(`parolni kiriting`);
-alert(`tabriklaymiz siz tizimga oddiy user bo'lib kirdingiz`);
-let adminLogin = prompt(`admin loginni kiriting`);
-let adminParol = prompt(`admin parolni kiriting`);
-alert(`tabriklaymiz siz tizimga admin bo'lib kirdingiz`);
+// let userLogin = prompt(`loginni kiriting`);
+// let userParol = prompt(`parolni kiriting`);
+// alert(`tabriklaymiz siz tizimga oddiy user bo'lib kirdingiz`);
+// let adminLogin = prompt(`admin loginni kiriting`);
+// let adminParol = prompt(`admin parolni kiriting`);
+// alert(`tabriklaymiz siz tizimga admin bo'lib kirdingiz`);
