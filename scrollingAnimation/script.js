1eeca3e7c763695fa6641741f10e6d7f115c8fcb
XusniddinNarkulov <narkulof@gmail.com>
2022-03-19 15:06:35 +0500

    sdkfjsd

--- scrollingAnimation/script.js
+++ scrollingAnimation/script.js
@@ -1,13 +1,19 @@
 "use strict";
 
 let btn = document.querySelectorAll(".button");
-btnPosition = btn.getBoundingClientRect().top;
+console.log(btn);
+let btnPosition;
 let vheight = window.innerHeight;
-window.addEventListener("scroll", function () {
-  if (btnPosition < vheight) {
-    btn.classList.add("active");
-  } else {
-    btn.classList.remove("active");
-  }
+btn.forEach(function (val) {
+  console.log(val.getBoundingClientRect());
+  btnPosition = val.getBoundingClientRect().top;
 });
+
+// window.addEventListener("scroll", function () {
+//   if (btnPosition < vheight) {
+//     btn.classList.add("active");
+//   } else {
+//     btn.classList.remove("active");
+//   }
+// });+
 console.log(window.innerHeight);
