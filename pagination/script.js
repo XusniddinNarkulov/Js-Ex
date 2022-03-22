279d7f6d8ba584a76a3c128a4529ee36c4432b61
Xusniddin <narkulof@gmail.com>
2022-03-22 08:38:49 +0500

    pagination html css

---
+++ pagination/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let prev = document.querySelector(".prev");
+let next = document.querySelector(".next");
+let pages = document.querySelector(".pages");
+let blueP = document.querySelector(".blueP");
+let blueDiv = document.querySelector(".blueDiv");
+let psibling = blueP.nextElementSibling;
+psibling.classList.add("blueDiv");
+next.addEventListener("click", function (e) {});
+// blueP.nextElementSibling.classList.add("blueDiv");
+// psibling.nextElementSibling.classList.add("blueP");
