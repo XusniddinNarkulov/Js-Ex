571ab6839ae286d0170c867d67f220acedc5a4fa
Xusniddin <narkulof@gmail.com>
2022-03-23 12:21:54 +0500

    setInterval followers

--- followersCounter/script.js
+++ followersCounter/script.js
@@ -4,17 +4,26 @@ console.log(p);
 let p1 = document.querySelector(".p1");
 let p2 = document.querySelector(".p2");
 let p3 = document.querySelector(".p3");
-// p.forEach((val) => {
-//   console.log(val);
-//   let el = 0;
-//   let sI = (val.textContent = setInterval(function () {
-//     val.textContent = el++;
-//     if (
-//       p[0].textContent === 1200 &&
-//       p[1].textContent === 500 &&
-//       p[2].textContent === 750
-//     ) {
-//       clearInterval(sI);
-//     }
-//   }, 1));
-// });
+let el1 = 0;
+let el2 = 0;
+let el3 = 0;
+let si1 = (p1.textContent = setInterval(function () {
+  p1.textContent = el1++;
+  if (p1.textContent == 1200) {
+    clearInterval(si1);
+  }
+}, 1));
+
+let si2 = (p2.textContent = setInterval(function () {
+  p2.textContent = el2++;
+  if (p2.textContent == 500) {
+    clearInterval(si2);
+  }
+}, 1));
+
+let si3 = (p3.textContent = setInterval(function () {
+  p3.textContent = el3++;
+  if (p3.textContent == 750) {
+    clearInterval(si3);
+  }
+}, 1));
--- pagination/script.js
+++ pagination/script.js
@@ -3,10 +3,25 @@
 let prev = document.querySelector(".prev");
 let next = document.querySelector(".next");
 let pages = document.querySelector(".pages");
+let page = pages.children;
+console.log(page);
 let blueP = document.querySelector(".blueP");
 let blueDiv = document.querySelector(".blueDiv");
 let psibling = blueP.nextElementSibling;
-psibling.classList.add("blueDiv");
-next.addEventListener("click", function (e) {});
+// console.log(pages.children);
+for (let i = 0; i < page.length; i++) {
+  next.addEventListener("click", function (e) {
+    if (page[i].classList.contains("blueP")) {
+      page[i].nextElementSibling.classList.add("blueDiv");
+    }
+  });
+}
+// psibling.classList.add("blueDiv");
+// next.addEventListener("click", function (e) {
+//   if (psibling) {
+//     psibling.classList.add("blueDiv");
+//     psibling.nextElementSibling.classList.add("blueP");
+//   }
+// });
 // blueP.nextElementSibling.classList.add("blueDiv");
 // psibling.nextElementSibling.classList.add("blueP");
