002bc18680be9b16b7ee64659dd46fce69ec0cdb
Xusniddin <narkulof@gmail.com>
2022-03-29 12:05:50 +0500

    asdasd

---
+++ Todo List/script.js
@@ -0,0 +1,5 @@
+"use strict";
+let input = document.querySelector("input");
+let add = document.querySelector("label");
+let del = document.querySelector("button");
+console.log(input.value);
---
+++ qoshuv/script.js
@@ -0,0 +1,8 @@
+"use strict";
+let first = document.querySelector(".first");
+let second = document.querySelector(".second");
+let input = document.querySelector("input");
+let check = document.querySelector("label");
+function r() {
+  return Math.round(Math.random() * 100);
+}
