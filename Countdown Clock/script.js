2fa5f1476339029c4f14bd3bb8398235fa1abbbf
Xusniddin <narkulof@gmail.com>
2022-03-29 11:53:12 +0500

    finished timer

--- Countdown Clock/script.js
+++ Countdown Clock/script.js
@@ -1,17 +1,45 @@
 "use strict";
 
-let day = document.querySelector(".day");
-let hour = document.querySelector(".hour");
-let minute = document.querySelector(".minute");
-let second = document.querySelector(".second");
+// let day = document.querySelector(".day");
+// let hour = document.querySelector(".hour");
+// let minute = document.querySelector(".minute");
+// let second = document.querySelector(".second");
 
-let s = 59;
-let si = setInterval(function () {
-  second.textContent = s--;
-  if (second.textContent < 0) {
-    minute.textContent = minute.textContent - 1;
-    second.textContent = 59;
-    clearInterval(si);
-    s--;
-  }
-}, 10);
+// let s = second.textContent;
+// let m = minute.textContent;
+// console.log();
+
+// let si = setInterval(function () {
+//   --s;
+//   if (s == 0) {
+//     --m;
+//     s = 60;
+//   }
+// }, 10);
+
+window.onload = function () {
+  let day = 1;
+  let hour = 1;
+  let minute = 5;
+  let sec = 60;
+  setInterval(function () {
+    document.getElementById("timer").innerHTML =
+      day + ` : ` + hour + ` : ` + minute + ` : ` + sec;
+    sec--;
+    if (sec == 0) {
+      minute--;
+      sec = 59;
+      if (minute == 0) {
+        hour--;
+        minute = 59;
+        if (hour == 0) {
+          day--;
+          hour = 23;
+          if ((day = 0)) {
+            clearInterval;
+          }
+        }
+      }
+    }
+  }, 1000);
+};
