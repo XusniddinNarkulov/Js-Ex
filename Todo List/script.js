2d5296ac451c808d3c3ee445ebd8fca44ebfcda5
XusniddinNarkulov <narkulof@gmail.com>
2022-03-29 15:22:00 +0500

    to do list

--- Todo List/script.js
+++ Todo List/script.js
@@ -1,5 +1,27 @@
 "use strict";
 let input = document.querySelector("input");
 let add = document.querySelector("label");
-let del = document.querySelector("button");
-console.log(input.value);
+
+let content = document.querySelector(".content");
+let contentChild = document.querySelector(".content__child");
+add.addEventListener("click", function () {
+  if (input.value == "") {
+    alert("matn kiriting");
+  } else {
+    content.innerHTML += `<div class="content__child">
+  <p>${input.value}</p>
+  <button>Delete</button>
+</div>`;
+    // if (input.value == "") {
+    //   alert(`error`);
+    // }
+    input.value = "";
+
+    let del = document.querySelectorAll("button");
+    for (let i = 0; i < del.length; i++) {
+      del[i].onclick = function () {
+        this.parentElement.remove();
+      };
+    }
+  }
+});
