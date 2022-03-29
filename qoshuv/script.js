669f551eb64e5caf1c23f1e8865e83e3ee3852e8
XusniddinNarkulov <narkulof@gmail.com>
2022-03-29 15:54:59 +0500

    qo'shuv

--- qoshuv/script.js
+++ qoshuv/script.js
@@ -3,6 +3,23 @@ let first = document.querySelector(".first");
 let second = document.querySelector(".second");
 let input = document.querySelector("input");
 let check = document.querySelector("label");
+let firstText = (first.textContent = r());
+let secondText = (second.textContent = r());
+
 function r() {
-  return Math.round(Math.random() * 100);
+  return Math.round(Math.random() * 10);
 }
+// console.log(typeof firstText);
+check.onclick = (e) => {
+  // e.preventDefault();
+
+  if (+input.value === +firstText + +secondText) {
+    alert(`to'g'ri topdingiz`);
+    location.reload();
+    // firstText = r();
+    // secondText = r();
+  } else {
+    location.reload();
+    alert(`incorrect \ to'g'ri javob:${+firstText + +secondText}`);
+  }
+};
