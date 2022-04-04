c7ce4911e5bae8d800281f3c96bbc7b46f8ae4c0
Xusniddin <narkulof@gmail.com>
2022-04-04 08:32:57 +0500

    asdsad

--- the meal/script.js
+++ the meal/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const input = document.querySelector("input");
+const search = document.querySelector("label");
+const notFind = document.querySelector(".notFind");
+const grid = document.querySelector(".grid");
+const getRecipes = document.getElementsByClassName("child__get-recipe");
+const getrecipes = document.querySelectorAll(".child__get-recipe");
+const pop = document.querySelector(".pop");
+const x = document.querySelector("#x");
+
+search.addEventListener("click", function () {
+  fetch();
+  if (input.value) {
+  }
+});
