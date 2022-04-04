54135cddabd6b56c6f83a43adad529fd8712289a
Xusniddin <narkulof@gmail.com>
2022-04-04 12:16:42 +0500

    asdfd

--- the meal/script.js
+++ the meal/script.js
@@ -9,8 +9,21 @@ const getrecipes = document.querySelectorAll(".child__get-recipe");
 const pop = document.querySelector(".pop");
 const x = document.querySelector("#x");
 
+function foodName(foodName) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
+    .then((e) => {
+      console.log(e);
+      return e.json();
+    })
+    .then((e) => {
+      console.log(e.meals[0]);
+      // let meals = e.meals;
+      // console.log(meals);
+    });
+}
+foodName("Arrabiata");
+
 search.addEventListener("click", function () {
-  fetch();
   if (input.value) {
   }
 });
