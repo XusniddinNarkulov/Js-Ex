1e7994c0cc5cf275d52bd9ba311bca8b1d625d0e
XusniddinNarkulov <narkulof@gmail.com>
2022-04-04 15:04:13 +0500

    lskdjfkd

--- the meal/script.js
+++ the meal/script.js
@@ -7,23 +7,76 @@ const grid = document.querySelector(".grid");
 const getRecipes = document.getElementsByClassName("child__get-recipe");
 const getrecipes = document.querySelectorAll(".child__get-recipe");
 const pop = document.querySelector(".pop");
-const x = document.querySelector("#x");
 
-function foodName(foodName) {
-  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
+// function foodName(foodName) {
+fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`)
+  .then((e) => {
+    console.log(e);
+    return e.json();
+  })
+  .then((e) => {
+    console.log(e.meals[0]);
+    let el = e.meals[0];
+    // let meals = e.meals;
+    // console.log(meals);
+    search.addEventListener("click", function (e) {
+      e.preventDefault();
+      if (input.value == "Arrabiata") {
+        grid.innerHTML += `
+        <div class="child">
+          <img class="child__img" src="${el.strMealThumb}" alt="" />
+          <h3 class="child__food-name">${el.strMeal}</h3>
+          <a class="child__get-recipe" href="">Get Recipe</a>
+        </div>
+        `;
+
+        pop.innerHTML = `
+        <h2 class="pop__x" id="x">❎</h2>
+        <h2 class="pop__food-name">${el.strMeal}</h2>
+        <p class="bg-white">${el.strCategory}</p>
+        <h3>Instructions</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere nisi
+          optio consequuntur, eaque quaerat quia nesciunt sapiente fuga quos
+          cupiditate reprehenderit earum, dignissimos aut sint sunt expedita
+          itaque quae amet.
+        </p>
+        <img src="${el.strMealThumb}" alt="" class="pop__img" />
+        <a href="">Watch Video</a>
+        `;
+
+        const x = document.querySelector("#x");
+
+        // pop.classList.add("visible");
+
+        getRecipes[0].addEventListener("click", function (e) {
+          e.preventDefault();
+          pop.classList.add("visible");
+        });
+
+        x.addEventListener("click", function (e) {
+          e.preventDefault();
+          pop.classList.remove("visible");
+        });
+
+        notFind.style.display = "none";
+      } else {
+        notFind.style.display = "block";
+      }
+    });
+  });
+// }
+// foodName("Arrabiata");
+
+function foodList() {
+  fetch(`www.themealdb.com/api/json/v1/1/search.php?f=a
+  `)
     .then((e) => {
-      console.log(e);
       return e.json();
     })
     .then((e) => {
-      console.log(e.meals[0]);
-      // let meals = e.meals;
-      // console.log(meals);
+      console.log(e);
+      return e;
     });
 }
-foodName("Arrabiata");
-
-search.addEventListener("click", function () {
-  if (input.value) {
-  }
-});
+foodList();
