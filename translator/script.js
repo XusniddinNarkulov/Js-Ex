59ab107f10e019761639e66f2f3b8b0310dd6ab7
Xusniddin <narkulof@gmail.com>
2022-04-07 12:46:17 +0500

    translator

---
+++ translator/script.js
@@ -0,0 +1,72 @@
+"use strict";
+
+let fromLang = document.querySelector("#from");
+let toLang = document.querySelector("#to");
+
+let fromInput = document.querySelector(".from__area");
+let toInput = document.querySelector(".to__area");
+
+let changer = document.querySelector(".changer");
+let btn = document.querySelector(".run__btn");
+
+let langs = [];
+let langsArr = [];
+
+btn.addEventListener("click", function () {
+  const encodedParams = new URLSearchParams();
+  encodedParams.append("q", fromInput.value);
+  encodedParams.append("target", toLang.value);
+  encodedParams.append("source", fromLang.value);
+
+  const options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      "Accept-Encoding": "application/gzip",
+      "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+      "X-RapidAPI-Key": "16c44be1efmsh44f5951daad4473p14f8d1jsndfeaf754abe0",
+    },
+    body: encodedParams,
+  };
+
+  fetch(
+    "https://google-translate1.p.rapidapi.com/language/translate/v2",
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      toInput = response.data.translations[0].translatedText;
+      console.log(response.data.translations[0].translatedText);
+    })
+    .catch((err) => console.error(err));
+  /**data:
+translations: Array(1)
+0: {translatedText: 'hello'}
+length: 1 */
+});
+
+const options = {
+  method: "POST",
+  headers: {
+    "content-type": "application/x-www-form-urlencoded",
+    "Accept-Encoding": "application/gzip",
+    "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+    "X-RapidAPI-Key": "16c44be1efmsh44f5951daad4473p14f8d1jsndfeaf754abe0",
+  },
+  body: encodedParams,
+};
+
+fetch(
+  "https://google-translate1.p.rapidapi.com/language/translate/v2/languages",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => {
+    langs = response.data.languages;
+    langs.forEach((element) => {
+      let opt = `<option value="${element.language}">${element.language}</option>`;
+      fromLang.insertAdjacentHTML("beforeend", opt);
+      toLang.insertAdjacentHTML("beforeend", opt);
+    });
+  })
+  .catch((err) => console.error(err));
